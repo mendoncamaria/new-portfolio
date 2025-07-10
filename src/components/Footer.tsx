c096439c031a76c4924d82914d5a2437f4d374e3
Maria Mendonca <maria.mendonca156@gmail.com>
2025-07-10 22:36:48 +0530

    Added components

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="w-full bg-gray-800 text-white text-center p-4">
+        <p>&copy; {new Date().getFullYear()} Maria Mendonca. All rights reserved.</p>
+      </footer>
+  );
+};
+
+export default Footer;
